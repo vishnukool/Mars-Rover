e8ac145a97208f6c9f68efa173bb4a8a41e795da
Vishnu k <vishnukool@gmail.com>
2011-07-10 14:30:41 +0530

    Created Direction and rover class

---
+++ src/CurrentDirection.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: vishnuk
+ * Date: 7/10/11
+ * Time: 2:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class CurrentDirection {
+
+    abstract public void turnLeft();
+
+    abstract public void turnRight();
+
+    abstract public void moveForward();
+
+}
---
+++ src/MarsRover.java
@@ -0,0 +1,15 @@
+import org.omg.PortableServer.Current;
+
+import javax.swing.text.Position;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vishnuk
+ * Date: 7/10/11
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MarsRover {
+    Position roverPosition;
+    CurrentDirection roverDirection;
+}

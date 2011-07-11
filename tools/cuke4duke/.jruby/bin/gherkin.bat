d9e5138dcf482cf040cf9cf4e67cb789300a0439
Vishnu k <vishnukool@gmail.com>
2011-07-11 23:43:28 +0530

    fixing project structure

---
+++ tools/cuke4duke/.jruby/bin/gherkin.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"jruby.exe" "d:/Mars Rover/tools/cuke4duke/.jruby/bin/gherkin" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"jruby.exe" "%~dpn0" %*

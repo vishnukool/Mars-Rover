8efc8bd50eb32e35f3e20c59394fdd0585a535ff
Vishnu k <vishnukool@gmail.com>
2011-07-13 02:28:02 +0530

    fixed integration testing ambiguity

--- src/test/integration/testExecuteCommandEffectOnDirectionChange.java
+++ src/test/integration/testExecuteCommandEffectOnDirectionChange.java
@@ -5,16 +5,17 @@ import mars.MarsRover;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class testExecuteCommandEffectOnDirectionChange {
     @Test
     public void testExecuteCommand() {
         MarsRover testingMarsRover=new MarsRover(2,4,'N');
         testingMarsRover.executeCommand("R");
+        MarsRover mockedMarsRover=mock(MarsRover.class);
 
-        CurrentMovingDirectionEast currentDirectionEast=new CurrentMovingDirectionEast(testingMarsRover);
+        CurrentMovingDirectionEast currentDirectionEast=new CurrentMovingDirectionEast(mockedMarsRover);
         assertTrue(testingMarsRover.getCurrentDirection().equals(currentDirectionEast));
-
 
     }
 
